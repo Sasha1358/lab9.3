28a5a5fd6422923ba17cf66d9e54487d7f868676
Sasha1358 <0551553733z@gmail.com>
2023-12-13 19:17:04 -1200

    Добавьте файлы проекта.

---
+++ UnitTest9,3/UnitTest9,3.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab9.3/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest93
+{
+	TEST_CLASS(UnitTest93)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Route array[] = { {{"A"}, {"B"}, 1} };
+			int size = 1;
+				Sort(array, size);
+				Assert::AreEqual(size, 1);
+		}
+	};
+}
